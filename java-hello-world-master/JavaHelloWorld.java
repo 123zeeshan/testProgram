7d9efa32f584a244e7a6d4c904c8779c4e429a9e
Muhammad Zeeshan <sunamundame5@gmail.com>
2017-04-05 04:05:58 -0700

    Add files via upload

---
+++ java-hello-world-master/JavaHelloWorld.java
@@ -0,0 +1,17 @@
+public class JavaHelloWorld
+{
+	public static void main(String [] args){
+		System.out.println("Java Hello World");
+		System.out.println("Hello again");
+		System.out.println("Hello again 01");
+		System.out.println("Hello again 02");
+		System.out.println("Hello again 03");
+		System.out.println("Hello again 04");
+		System.out.println("Hello again 05");
+		System.out.println("Hello again 06");
+		System.out.println("Hello again 07");
+		System.out.println("Hello again 08");
+		System.out.println("Hello again 09");
+		System.out.println("time to say bye now.. ");
+	}
+}
